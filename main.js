61580ff780743cc7da863f98d325479bf0319c5a
Aaron Claude Lacap <aaronclaude0028@gmail.com>
2024-03-29 10:52:56 +0800

    updated website
    - added counter section
    - fixed the bugs on the positioning of navbar
    - added navbar hide/show effect on scroll
    - fixed poppins font
    - added different types of font weight of poppins font

--- main.js
+++ main.js
@@ -6,3 +6,54 @@ document.addEventListener('DOMContentLoaded', function () {
         wrap: true // Enable wrapping of slides (i.e., go from last to first slide and vice versa)
     });
 });
+
+// JavaScript function to start the counter animation when the element comes into view
+function startCounters() {
+    const counters = document.querySelectorAll('.count');
+
+    const options = {
+        threshold: 0.6 // Adjust this threshold value as needed
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const target = entry.target;
+                const targetCount = parseInt(target.getAttribute('data-target'));
+                const updateCount = () => {
+                    const count = parseInt(target.innerText);
+                    const increment = targetCount / 100; // Increase the number of increments for a smoother animation
+                    if (count < targetCount) {
+                        target.innerText = Math.ceil(count + increment);
+                        setTimeout(updateCount, 10); // Adjust the duration of each increment (milliseconds)
+                    } else {
+                        target.innerText = targetCount;
+                    }
+                };
+                updateCount();
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    counters.forEach(counter => {
+        observer.observe(counter);
+    });
+}
+
+// Call the function when the page is fully loaded
+window.addEventListener('load', startCounters);
+
+
+// JavaScript function to hide/show the navbar on scroll
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("header").style.top = "0";
+    } else {
+        document.querySelector("header").style.top = "-75px"; // Adjust the height of the navbar as needed
+    }
+    prevScrollpos = currentScrollPos;
+}
